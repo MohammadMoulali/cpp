d42a5185ac3e21ed6cd84ea7a881b78a90da686c
MohammadMoulali <116294232+MohammadMoulali@users.noreply.github.com>
2023-03-02 21:44:16 +0530

    Create oploading.cpp

---
+++ oploading.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class s
+{
+    int x,y;
+    public:
+    void getdata()
+    {
+        cout<<"enter two values"<<endl;
+        cin>>x>>y;
+    }
+    void display()
+    {
+        cout<<x<<"\t"<<y<<endl;
+        
+    }
+    friend void operator-(s &m);/*we also use friend in operator overloading when parameters are pass by reference*/
+};
+void operator-(s &m)
+{
+   m.x=-m.x;
+    m.y=-m.y;
+}
+int main()
+{
+    s m;
+    m.getdata();
+    m.display();
+    -m;
+    m.display();
+    return 0;
+    
+}
